893113bf6a4b33832fb1547831a99a6283c2349e
Student <venus.kabera@student.moringaschool.com>
2023-02-24 09:59:30 +0300

    done

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,7 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    # Your code her
+      add_column :characters, :catchphrase, :string
+    end
   end
-end
+
